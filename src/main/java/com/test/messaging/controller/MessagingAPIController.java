8fc0aba06c8f07430fb107b71c4eab71366c3829
Akshay Sreekumar <51793325+Akshaysreekumar123@users.noreply.github.com>
2020-10-10 21:32:23 +0530

    Add files via upload

--- src/main/java/com/test/messaging/controller/MessagingAPIController.java
+++ src/main/java/com/test/messaging/controller/MessagingAPIController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,19 +20,22 @@ import com.test.messaging.service.MessageService;
 public class MessagingAPIController {
 	@Autowired
 	MessageService msgService;
+	
+	@CrossOrigin(origins = "*")
 	@RequestMapping(value="/message", method=RequestMethod.GET, produces= MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<Message>> retrieveMessages()
 	{
 		return new ResponseEntity<List<Message>> (msgService.findAllMessages(), HttpStatus.OK);
 	}
 	
+	@CrossOrigin(origins = "*")
 	@RequestMapping(value="/message/{id}", method=RequestMethod.GET, produces= MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Message> retrieveMessage(@PathVariable String id)
 	{
 		return new ResponseEntity<Message> (msgService.findMessage(id), HttpStatus.OK);
 	}
 
-
+	@CrossOrigin(origins = "*")
 	@RequestMapping(value="/message", method=RequestMethod.POST, produces= MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Message> createMessage(@RequestBody Message msg)
 	{
