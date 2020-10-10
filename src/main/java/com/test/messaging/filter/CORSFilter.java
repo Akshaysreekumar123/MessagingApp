239e9327f5883aa6f450f08f7ff724f058252e62
Akshay Sreekumar <51793325+Akshaysreekumar123@users.noreply.github.com>
2020-10-10 21:31:42 +0530

    Add files via upload

---
+++ src/main/java/com/test/messaging/filter/CORSFilter.java
@@ -0,0 +1,24 @@
+package com.test.messaging.filter;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+public class CORSFilter implements javax.servlet.Filter{
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+
+		HttpServletResponse res = (HttpServletResponse) response;
+		res.addHeader("Access-Control-Allow-Origin", "*");
+		res.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+		res.addHeader("Access-Control-Allow-Headers", "Content-Type");
+		chain.doFilter(request, response);
+	}
+
+}
